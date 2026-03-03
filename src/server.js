96ddc351fc8a3e4a7f5353f738ae1cfae5b7e6f1
AnnaRoska <murkesha2014@gmail.com>
2026-03-03 18:33:19 +0200

    Create HW02-Ver01

--- src/server.js
+++ src/server.js
@@ -1,57 +1,40 @@
 import 'dotenv/config';
 import cors from 'cors';
 import express from 'express';
-import pinoHttp from 'pino-http';
+import { connectMongoDB } from './db/connectMongoDB.js';
+
+import { logger } from './middleware/logger.js';
+import { notFoundHandler } from './middleware/notFoundHandler.js';
+import { errorHandler } from './middleware/errorHandler.js';
+import notesRoutes from './routes/notesRoutes.js';
+
+
 
 const app = express();
-
-app.use(cors());
-app.use(express.json());
-app.use(
-  pinoHttp({
-    level: 'info',
-    transport: {
-      target: 'pino-pretty',
-      options: {
-        colorize: true,
-        ignore: 'pid,hostname',
-        messageFormat: '{req.method} {req.url} {res.statusCode} - {responseTime}ms',
-      },
-    },
-  }),
-);
-
-app.get('/notes', (_req, res) => {
-  res.status(200).json({ message: 'Retrieved all notes' });
-});
-
-app.get('/notes/:noteId', (req, res) => {
-  const id = Number.parseInt(req.params.noteId, 10);
-
-  if (Number.isNaN(id)) {
-    res.status(400).json({ message: 'Please provide a valid ID' });
-    return;
-  }
-    res.status(200).json( { message: `Retrieved note with ID: ${id}` } );
-});
-
-app.get('/test-error', () => {
-  throw new Error('Simulated server error');
-});
-
-app.use((req, res, next) => {
-  res.status(404).json({ message: 'Route Not Found' });
-});
-
-
-app.use((error, req, res, next) => {
-  res.status(500).json({
-    message: error.message,
-  });
-});
-
 const PORT = Number.parseInt(process.env.PORT, 10) || 3000;
 
+// Глобальні middleware
+app.use(logger);         // 1. Логер першим — бачить усі запити
+app.use(express.json({
+  type: ['application/json', 'application/vnd.api+json'],
+}));                     // 2. Парсинг JSON-тіла
+app.use(cors());         // 3. Дозвіл для запитів з інших доменів
+
+
+// підключаємо групу маршрутів для роботи з нотатками
+app.use(notesRoutes);
+  
+// 404 — якщо маршрут не знайдено
+app.use(notFoundHandler);
+
+// Error — якщо під час запиту виникла помилка
+app.use(errorHandler);
+
+
+// підключення до MongoDB
+await connectMongoDB();
+
+// запуск сервера
 app.listen(PORT, (error) => {
   if (error) {
     throw error;
